20bc870e4df37c373c17349f9b01245e51cae4e6
Alex Rodriguez <ardzoht@gmail.com>
2014-11-06 12:26:24 -0700

    Initial Commit

---
+++ BluetoothTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BluetoothTest
+//
+//  Created by CITA on 11/6/14.
+//  Copyright (c) 2014 itesm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
